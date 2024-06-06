006085e501cad830361e5bf23d72f00fdeb61efd
quazistellar <gni7777@yandex.ru>
2024-06-06 11:27:52 +0300

    сохранила все

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,242 @@
+﻿using Microsoft.Win32;
+using Spire.Xls;
+using Spire.Xls.Core;
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+
+namespace moffice_9laba
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MinWidth = 500;
+            MinHeight = 350;
+        }
+
+        private void new_Click(object sender, RoutedEventArgs e)
+        {
+            Workbook workbook = new Workbook();
+            workbook.Worksheets.Clear();
+            Worksheet sheet = workbook.Worksheets.Add("новый листик");
+            var dataview = grid.ItemsSource as DataView;
+
+            //if (dataview != null)
+            //{
+            //    sheet.InsertDataView(dataview, true, 1, 1);
+            //}
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel Files|.xls;.xlsx;*.xlsm";
+            saveFileDialog.Title = "Куда сохранить Excel-файл?";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                workbook.SaveToFile(filePath, FileFormat.Version2010);
+                Workbook loadedWorkbook = new Workbook();
+                workbook.Worksheets.Clear();
+                loadedWorkbook.LoadFromFile(filePath);
+                Worksheet loadedSheet = loadedWorkbook.Worksheets[0];
+                var loadedDataView = loadedSheet.ExportDataTable().DefaultView;
+                grid.ItemsSource = loadedDataView;
+            }
+        }
+
+
+        private void ClearGrid()
+        {
+            try
+            {
+                DataTable dt = ((DataView)grid.ItemsSource).Table;
+                dt.Clear();
+                grid.ItemsSource = null;
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("датагрида и так нет!");
+            }
+        }
+
+        private void ClearTable()
+        {
+            try
+            {
+                DataTable dt = ((DataView)grid.ItemsSource).Table;
+                dt.Clear();
+                grid.ItemsSource = null;
+                grid.ItemsSource = dt.DefaultView;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("таблицы и так нет!");
+            }
+        }
+
+        private void exist_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            openFileDialog.Title = "Выберите Excel-файл для работы с ним";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+                string filePath = openFileDialog.FileName;
+                Workbook workbook = new Workbook();
+                workbook.Worksheets.Clear();
+                workbook.LoadFromFile(filePath);
+                Worksheet sheet = workbook.Worksheets[0];
+                CellRange range = sheet.AllocatedRange;
+                var datatable = sheet.ExportDataTable(range, true);
+                grid.ItemsSource = datatable.DefaultView;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                AddColumns();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при добавлении столбца");
+            }
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            if (grid.ItemsSource == null)
+            {
+                MessageBox.Show("Нет данных для сохранения");
+                return;
+            }
+            
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            saveFileDialog.Title = "Сохранить Excel-файл";
+
+            try
+            {
+                if (File.Exists(saveFileDialog.FileName))
+                {
+                    Workbook workbook = new Workbook();
+                    workbook.Worksheets.Clear();
+                    Worksheet sheet = workbook.Worksheets[0];
+                    var dataview = grid.ItemsSource as DataView;
+                    sheet.InsertDataView(dataview, true, 1, 1);
+                    workbook.SaveToFile(saveFileDialog.FileName, FileFormat.Version2010);
+                }
+                else
+                {
+                    if (saveFileDialog.ShowDialog() == true)
+                    {
+                        Workbook workbook = new Workbook();
+                    workbook.Worksheets.Clear();
+                        Worksheet sheet = workbook.Worksheets.Add("новый листик");
+                        var dataview = grid.ItemsSource as DataView;
+                        sheet.InsertDataView(dataview, true, 1, 1);
+                        workbook.SaveToFile(saveFileDialog.FileName, FileFormat.Version2010);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при сохранении: " + ex);
+            }
+        }
+
+        private void send_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            openFileDialog.Title = "Выберите Excel-файл для отправки";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SendEmail send = new SendEmail(openFileDialog.FileName);
+                send.ShowDialog();
+            }
+        }
+
+        private void AddColumns()
+        {
+            DataTable dt = ((DataView)grid.ItemsSource).Table;
+            string name = namecol.Text;
+
+            if (!dt.Columns.Contains(name))
+            {
+                dt.Columns.Add(name);
+            }
+
+            DataView dataView = dt.DefaultView;
+            grid.ItemsSource = null;
+            grid.ItemsSource = dataView;
+        }
+
+        private void DeleteRows()
+        {
+            if (grid.SelectedItem != null)
+            {
+                DataRowView selectedRow = (DataRowView)grid.SelectedItem;
+                DataTable dt = ((DataView)grid.ItemsSource).Table;
+                dt.Rows.Remove(selectedRow.Row);
+            }
+            else
+            {
+                MessageBox.Show("Ошибка при удалении строчки");
+            }
+        }
+
+       
+
+        private void delrow_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DeleteRows();
+            }
+            catch
+            {
+                MessageBox.Show("ошибка при удалении строчки");
+            }
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            var window = GetWindow(this);
+
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ClearGrid();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ClearTable();
+        }
+    }
+}
---
+++ SendEmail.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace moffice_9laba
+{
+    /// <summary>
+    /// Логика взаимодействия для SendEmail.xaml
+    /// </summary>
+    public partial class SendEmail : Window
+    {
+        private string _filePath;
+        public SendEmail(string filePath)
+        {
+            InitializeComponent();
+            _filePath = filePath;
+
+            MinHeight = 200;
+            MinWidth = 400;
+        }
+
+        private void send_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage(log.Text, forr.Text, theme.Text, "письмо");
+
+                if (File.Exists(_filePath))
+                {
+                    mail.Attachments.Add(new Attachment(_filePath));
+                }
+
+                else
+                {
+                    MessageBox.Show("Выбран неверный файл!");
+                }
+
+                SmtpClient smtp = new SmtpClient();
+
+                if (log.Text.Contains("@mail.ru"))
+                {
+                    smtp.Host = "smtp.mail.ru";
+                    smtp.Port = 587;
+                }
+                else if (log.Text.Contains("@yandex.ru"))
+                {
+                    smtp.Host = "smtp.yandex.ru";
+                    smtp.Port = 25;
+                }
+                else if (log.Text.Contains("@rambler.ru"))
+                {
+                    smtp.Host = "smtp.rambler.ru";
+                    smtp.Port = 25;
+                }
+                else if (log.Text.Contains("@gmail.com"))
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                }
+                else
+                {
+                    MessageBox.Show("Неверный хост или домен");
+                }
+
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential(log.Text, pass.Text);
+
+                try
+                {
+                    smtp.Send(mail);
+                    MessageBox.Show("Письмо успешно отправлено!");
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при отправке письма: " + ex);
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Вы не ввели данные для отправки!");
+            }
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            var window = GetWindow(this);
+
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
+    }
+}
